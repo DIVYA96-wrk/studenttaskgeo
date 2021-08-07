969ab99fa04c35ea2fab015a9f83491c1821865a
DIVYA MARGARET <16677@geojittechnologies.com>
2021-08-07 17:41:04 +0530

    register data binding

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  Gender="";
+  cusname="";
+  cuspass="";
+  onOptionsSelected: any;
 
-  constructor() { }
+
+  class=["Class 1", "Class2 ","Class 3" , "Class 4"]
+
+  constructor(private route:Router) { }
 
   ngOnInit(): void {
   }
@@ -15,5 +23,10 @@ export class RegisterComponent implements OnInit {
   register(){
     alert("Registered Succesfully!")
   }
+  back(){
+    this.route.navigateByUrl('login')
+  }
+  
+ 
 
 }
